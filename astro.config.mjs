7556f304d443c514de4e5d6084c1cbbf4fc84df6
BryanHogan <nayrb@gmx.de>
2024-03-17 20:51:41 +0100

    did customization

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,24 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://webdev.bryanhogan.com',
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
+			title: 'Web Dev',
+			logo: {
+				src: './src/assets/Penguin-Web-Dev-Bryan-Hogan-Logo.svg',
 			},
+			social: {
+				github: 'https://github.com/BryanHogan/webdev',
+				instagram: 'https://www.instagram.com/bryanhoganme/',
+				mastodon: "https://mastodon.social/@BryanHogan",
+				threads: "https://www.threads.net/@bryanhoganme",
+				// this should be BlueSky -> twitter: "https://bsky.app/profile/bryanhogan.com",
+				rss: "https://bryanhogan.com/rss.xml"
+			},
+			editLink: {
+				baseUrl: 'https://github.com/bryanhogan/webdev/edit/master/',
+			  },
 			sidebar: [
 				{
 					label: 'Guides',
