e3a4eefaf7997114e2a9c50430cb7096ea0938dc
BryanHogan <nayrb@gmx.de>
2024-04-30 22:48:03 +0200

    wrote seo pages content

--- astro.config.mjs
+++ astro.config.mjs
@@ -38,41 +38,42 @@ export default defineConfig({
 						{ label: "Components", link: "components/introduction/"},
 						//{ label: "NavBar", link: "components/navbar/", badge: "new" },
 						{ label: "Accordion", link: "components/accordion/", badge: "new" },
-						{ label: "NavBar", link: "components/navbar/", badge: { text: "wip", variant: "note" } },
-						{ label: "Table", link: "components/table/", badge: { text: "wip", variant: "note" } },
-						{ label: "Blockquote", link: "components/blockquote/", badge: { text: "wip", variant: "note" } },
+						{ label: "NavBar", link: "components/navbar/", badge: { text: "wip", variant: "caution" } },
+						{ label: "Table", link: "components/table/", badge: { text: "wip", variant: "caution" } },
+						{ label: "Blockquote", link: "components/blockquote/", badge: { text: "wip", variant: "caution" } },
 					],
 				},
 				{
 					label: "Visual Design & UX ✨",
 					items: [
-						{ label: "Laws Of UI", link: "visualdesign/ui-laws/", badge: { text: "wip", variant: "note" } },
-						{ label: "Picking Colors", link: "visualdesign/colors/", badge: { text: "wip", variant: "note" } },
+						{ label: "Laws Of UI", link: "visualdesign/ui-laws/", badge: { text: "wip", variant: "caution" } },
+						{ label: "Picking Colors", link: "visualdesign/colors/", badge: { text: "wip", variant: "caution" } },
 					],
 				},
 				{
 					label: "CSS 🎨",
 					items: [
-						{ label: "CSS Reset", link: "css/css-reset/", badge: { text: "wip", variant: "note" } },
-						{ label: "Spacing", link: "css/spacing/", badge: { text: "wip", variant: "note" } },
+						{ label: "CSS Reset", link: "css/css-reset/", badge: { text: "wip", variant: "caution" } },
+						{ label: "Spacing", link: "css/spacing/", badge: { text: "wip", variant: "caution" } },
 					],
 				},
 				{
 					label: "SEO 🔍",
 					items: [
-						{ label: "SEO", link: "seo/introduction/", badge: { text: "wip", variant: "note" } }
+						{ label: "SEO", link: "seo/introduction/", badge: "new"},
+						{ label: "<Head>", link: "seo/head/", badge: { text: "new", variant: "caution" } }
 					],
 				},
 				{
 					label: "Astro Components",
 					items: [
-						{ label: "Astro Components", link: "astro/introduction/", badge: { text: "wip", variant: "note" } },
+						{ label: "Astro Components", link: "astro/introduction/", badge: { text: "wip", variant: "caution" } },
 					],
 				},
 				{
 					label: "Miscellaneous",
 					items: [
-						{ label: "About", link: "miscellaneous/about/", badge: { text: "draft", variant: "note" } },
+						{ label: "About", link: "miscellaneous/about/", badge: { text: "draft", variant: "caution" } },
 						{ label: "Resources", link: "miscellaneous/resources/"}
 					]
 				},
